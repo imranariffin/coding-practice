d1e1cbc350247843e9516f5b47dad1a1bfa9b7c5
imranariffin <imran.ariffin@mail.utoronto.ca>
2016-10-03 17:27:16 -0400

    longest palindrome java

---
+++ 409/LongestPalindrome.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+class LongestPalindrome {
+	HashMap<Character, Integer> counter;
+
+	public int longestPalindrome(String s) {
+		int ret = 0;
+
+		counter = new HashMap<Character, Integer>();
+
+		// count char occurences
+		for (Character c: s.toCharArray()) {
+			if (counter.containsKey(c))
+				counter.put(c, counter.get(c)+1);
+			else 
+				counter.put(c, 1);
+		}
+
+		// count even and odd chars
+		int n_odd = 0;
+		for (Character c: counter.keySet()) {
+			int f = counter.get(c);
+			if (f%2 == 0)
+				ret += f;
+			else {
+				ret += f-1;
+				n_odd += 1;
+			}
+		}
+
+		// find max odd chars
+		int max_f = 0;
+		char max_char;
+		for (Character c: counter.keySet()) {
+			int f = counter.get(c);
+			if (f%2 == 1)
+				if (f > max_f) {
+					max_f = f;
+					max_char = c;
+				}
+		}
+
+		if (n_odd > 0)
+			ret += 1;
+		return ret;
+		}
+	}
