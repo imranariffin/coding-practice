70c87040b79df00a178b7f005dd8686adb532416
cglotrr2 <cglotrr2@gmail.com>
2016-10-19 22:00:57 +0800

    Optimize solution from 3% to 24%

--- 293/cg_leetcode_293_solution_3p.java
+++ 293/cg_leetcode_293_solution_3p.java
@@ -3,11 +3,10 @@ public class Solution {
         ArrayList<String> res = new ArrayList<String>();
         
         for (int i = 0; i < s.length() - 1; i++) {
-            Character c1 = s.charAt(i);
-            Character c2 = s.charAt(i + 1);
-            
-            if (c1 == '+' && c2 == '+') {
-                res.add(s.substring(0, i) + "--" + s.substring(i + 2));
+            if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
+                String tmp = s.substring(0, i) + "--" + s.substring(i + 2);
+                
+                res.add(tmp);
             }
         }
         
