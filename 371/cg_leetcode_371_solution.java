938c30b185c701a19a3b55df88eeb93ef38183e0
cglotrr2 <cglotrr2@gmail.com>
2016-10-19 23:18:24 +0800

    Add LeetCode 371 Java solution Elegant solution using clever bit manipulation. Solution is based on zhaolz's original solution in discussion.

---
+++ 371/cg_leetcode_371_solution.java
@@ -0,0 +1,21 @@
+// Refer "https://discuss.leetcode.com/topic/49771/java-simple-easy-understand-solution-with-explanation".
+public class Solution {
+    public int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        
+        if (b == 0) {
+            return a;
+        }
+        
+        while (b != 0) {
+            int carry = a & b;
+            
+            a = a ^ b;
+            b = carry << 1;
+        }
+        
+        return a;
+    }
+}
