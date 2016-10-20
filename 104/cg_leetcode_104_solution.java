4f52f9b1f744a361ed87fb8c5022c3d43c48e275
ariffia <cglotrr2@gmail.com>
2016-10-20 11:53:16 +0800

    Add LeetCode 104 Java solution recursive

---
+++ 104/cg_leetcode_104_solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        return recurse(root, 0);
+    }
+    
+    private int recurse(TreeNode node, int d) {
+        if (node == null) {
+            return d;
+        }
+        
+        return max(recurse(node.left, d + 1), recurse(node.right, d + 1));
+    }
+    
+    private int max(int a, int b) {
+        if (a >= b) {
+            return a;
+        }
+        
+        return b;
+    }
+}
