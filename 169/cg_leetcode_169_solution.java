1fa10f723276e9d3d7698029672fda72fc70e33a
cglfai <cglotrr2@gmail.com>
2016-10-23 21:14:18 +0800

    Add LeetCode 169 Java solution

---
+++ 169/cg_leetcode_169_solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        
+        int mid = (nums.length - 1) / 2;
+        
+        return nums[mid];
+    }
+}
