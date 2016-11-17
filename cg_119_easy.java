01f0da23317a31610967ac060e68370157042405
ariffia <cglotrr2@gmail.com>
2016-11-17 08:06:09 +0800

    Shorten code

--- cg_119_easy.java
+++ cg_119_easy.java
@@ -2,9 +2,7 @@ public class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> res = new ArrayList<>();
         
-        res.add(1);
-        
-        for (int i = 1; i <= rowIndex; i++) {
+        for (int i = 0; i <= rowIndex; i++) {
             res.add(0, 1);
             
             for (int j = 2; j < res.size(); j++) {
