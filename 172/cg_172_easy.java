ee6edf006b2ee414854176f698d912307548b168
ariffia <cglotrr2@gmail.com>
2016-11-17 11:53:21 +0800

    Update comment

--- 172/cg_172_easy.java
+++ 172/cg_172_easy.java
@@ -4,12 +4,14 @@ public class Solution {
          * Refer "https://discuss.leetcode.com/topic/6516/my-one-line-solutions-in-3-languages".
          * 
          * Consider 100! = 100 * 99 * .. * 75 * .. * 25 * .. * 5 * .. * 1.
-         * And 100 / 5 = 20, 20 / 5 = 4, and 4 / 5 = 0.
-         * 100 / 5 = 5, 10, 15, 20, .. , 100.
-         * 20 / 5 = 5, 10, 15, 20.
          * 
-         * 100! contains 5 multiple of 15, 5 multiple of 5 and number 5.
-         * The reason we are interested in number 5 is because a trailing zero is produced when we multiple multiple of 5
+         * 100 / 5 = 20, 20 / 5 = 4, and 4 / 5 = 0.
+         * 
+         * 100 / 5 = 20 (5, 10, 15, 20, .. , 100).  <-- OK.
+         * 20 / 5 = 4 (5, 10, 15, 20).  <-- Why?
+         * 4 / 5 = 0 ().  <-- Why?
+         * 
+         * The reason we are interested in number 5 is because a trailing zero is produced when we multiply a multiple of 5
          * with an even number. Since even numbers are plentiful in 100!, we just assume we will have enough
          * of them to pair up with our multiple of 5s.
          */
