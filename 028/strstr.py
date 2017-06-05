bf5aafbaf2bc6db969da0b66a2db921ba9b2334f
Imran Ariffin <ariffinimran@gmail.com>
2017-06-05 19:04:23 -0400

    haystack simpler

--- 028/strstr.py
+++ 028/strstr.py
@@ -5,22 +5,8 @@ class Solution(object):
 		:type needle: str
 		:rtype: int
 		"""
-		i = 0
-		j = len(needle)
-		l = len(haystack)
-
-		if j > l:
-			return -1
-
-		for i in range(l-j+1):
-			print haystack[i:j]
-
-			if needle == haystack[i:j]:
-				return i
-
-			i += 1
-			j += 1
-
+		if needle in haystack:
+			return haystack.index(needle)
 		return -1
 
 if __name__=="__main__":
