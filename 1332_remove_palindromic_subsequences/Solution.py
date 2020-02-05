3f481103e1fd0c4cd237f8970054dc4dfdc2b037
Imran Ariffin <ariffin.imran@gmail.com>
2020-02-04 20:43:34 -0500

    LWC 143: 1332 Remove palindromic subseq

---
+++ 1332_remove_palindromic_subsequences/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        l = 0
+        r = len(s) - 1
+
+        if not s:
+            return 0
+
+        while l < r:
+            if s[l] != s[r]:
+                return 2
+
+            l += 1
+            r -= 1
+
+        return 1
+
