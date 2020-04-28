f8f947bbbf2016eea1141f480578f91f3aad01ba
Imran Ariffin <ariffin.imran@gmail.com>
2020-04-27 23:39:36 -0400

    LC 30D Challenge Longest Common Seq Bottom Up

--- 0000-lc30dc2020/w4d5_longest_common_sequence.py
+++ 0000-lc30dc2020/w4d5_longest_common_sequence.py
@@ -39,3 +39,28 @@ class SolutionDpTopDownLRU:
 
         return lcs(len(text1) - 1, len(text2) - 1)
 
+
+class SolutionDpBottomUp:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        memo = [
+            [0 for _ in range(n + 1)]
+            for _ in range(m + 1)
+        ]
+
+        for i, row in enumerate(memo):
+            if i == 0:
+                continue
+            for j, cell in enumerate(row):
+                if j == 0:
+                    continue
+                if text1[i - 1] == text2[j - 1]:
+                    memo[i - 1][j - 1] = 1 + memo[i - 2][j - 2]
+                else:
+                    memo[i - 1][j - 1] = max(
+                        memo[i - 1][j - 2],
+                        memo[i - 2][j - 1],
+                    )
+
+        return memo[m - 1][n - 1]
